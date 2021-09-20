2ff21f7495ad47cd40e3fe2d7510dea9c0c6514c
Riyo Wicaksono <stryuw@gmail.com>
2021-09-20 10:55:04 +0800

    datepicker

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -273,7 +273,7 @@ class OrderController extends Controller
                 'url' => $baseApiUrl . '/messages',
                 'method' => 'POST',
                 'payload' => json_encode([
-                    'message' => 'https://ptsp.joglohub.com/images/qr/' . $invoice . '.png',
+                    'message' => $gambar,
                     'phone_number' => $hp,
                     'message_type' => 'image',
                     'device_id' => 'redminote',
