798bf0e8d4f3b00836a43c7afe6675c6582f34b5
Riyo Wicaksono <stryuw@gmail.com>
2021-09-21 00:19:35 +0800

    daftar tarif

--- resources/views/front/sukses.blade.php
+++ resources/views/front/sukses.blade.php
@@ -116,7 +116,10 @@ Monitoring:  {{ $data->status }}
                                     <td class="px-4 py-3">{{ $p->namalayanan }}</td>
                                     <td class="px-4 py-3">{{ $p->jenislayanan }}</td>
                                     <td class="px-4 py-3">{{ $data->totalperiode }} @if ($p->satuan !== 'series')
-                                        {{  $p->satuan }} @else {{  $data[1] = explode(" ",$p->category->satuan) }}
+                                        {{  $p->satuan }} @else @php
+                                            $data = explode(" ",$p->category->satuan);
+                                            {{ $data }}
+                                        @endphp
                                     @endif</td>
                                     <td class="px-4 py-3">{{ number_format($p->tarif, 2) }}</td>
                                     <td class="px-4 py-3 text-right">Rp.{{ number_format($p->tarif * $data->totalperiode, 2,',','.') }}</td>
